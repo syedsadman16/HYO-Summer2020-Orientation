1c2815f0ada153de2d02fef4cedd43e8f003ed19
Kenny <oseleonose@gmail.com>
2020-06-05 23:08:23 -0700

    basic web server works

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/static/html/'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(process.env.PORT || 1337);

df57960b4d33a4810bc4d8678defc5c1c239d337
Kenny <oseleonose@gmail.com>
2020-06-07 23:01:40 -0700

    finished

--- static/javascript/timer.js
+++ static/javascript/timer.js
@@ -1,8 +1,13 @@
 function switchTimer(evt, timer) {
-   let timerTabs;
+   let timerTabs, tabLinks;
    timerTabs = document.getElementsByClassName("tabContent");
    for (let i = 0; i < timerTabs.length; i++) {
       timerTabs[i].style.display = "none";
    }
+   tabLinks = document.getElementsByClassName("tabLinks");
+   for (let i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+   }
    document.getElementById(timer).style.display = "block";
+   evt.currentTarget.className += " active";
 }

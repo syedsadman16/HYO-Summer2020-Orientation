bd5c9ea0361876f3d547dbde2037f19d22bf27c1
Syed Sadman <syedsadman16@gmail.com>
2020-06-09 19:58:21 -0400

    Appended time to list items

--- static/javascript/todo.js
+++ static/javascript/todo.js
@@ -1,10 +1,22 @@
 
 
-// Appends item to ordered list
-function addListItem(input) {
+function addListItem(input, time) {
   let list = document.getElementById("list");
   let item = document.createElement("li");
+
+  // Append text input to list item
   item.appendChild(document.createTextNode(input));
+
+  // Create span with time 
+  var span = document.createElement("SPAN");
+  var timeText = document.createTextNode(time + " minutes");
+  span.className = "time";
+  span.appendChild(timeText);
+
+  // Append span to list item
+  item.appendChild(span);
+
+  // Add all the items to list
   list.appendChild(item);
   return;
 }
@@ -14,8 +26,14 @@ function addTaskButtonClicked() {
   let text = document.getElementById("newTask").value;
   console.log(text);
   if (text != "") {
-    addListItem(text);
+    
+    /* Add pomodoro time clicked logic here */
+
+    addListItem(text, 25); // Default is 25, pass in pomo time instead
     document.getElementById("newTask").value = "";
+  }
+  else {
+    alert("Field cannot be empty");
   }
   return;
 }

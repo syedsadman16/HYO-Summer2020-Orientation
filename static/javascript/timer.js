57e8fc36b64d556dfb241c3289efcba35f2c34d1
Kenny <oseleonose@gmail.com>
2020-06-10 00:09:52 -0700

    basic timer not displayed

--- static/javascript/timer.js
+++ static/javascript/timer.js
@@ -8,7 +8,19 @@ function switchTimer(evt, timer) {
    for (let i = 0; i < tabLinks.length; i++) {
       tabLinks[i].className = tabLinks[i].className.replace(" active", "");
    }
-   document.getElementById(timer).style.display = "block";
+   document.getElementById("lBreak).style.display = "block";
    evt.currentTarget.className += " active";
+}
+
+
+function startTimer(numPomodoros) {
+   let onePomo = 25 * 60000;
+   let end = Date.now() + onePomo * numPomodoros;
+
+   setInterval(()=> {
+      let delta = end - Date.now(); 
+      document.getElementById("mins").innerHTML = Math.floor(delta / (1000 * 60) % 60).toString();
+      document.getElementById("secs").innerHTML = Math.floor((delta / 1000) % 60);
+   }, 100);
 }
 

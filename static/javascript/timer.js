3aad5f982efaf916098165e4653ad002712c05c0
Kenny <oseleonose@gmail.com>
2020-06-10 00:46:46 -0700

    timer works

--- static/javascript/timer.js
+++ static/javascript/timer.js
@@ -1,3 +1,5 @@
+var interval;
+
 function switchTimer(evt, timer) {
    let timerTabs, tabLinks;
    timerTabs = document.getElementsByClassName("tabContent");
@@ -8,19 +10,24 @@ function switchTimer(evt, timer) {
    for (let i = 0; i < tabLinks.length; i++) {
       tabLinks[i].className = tabLinks[i].className.replace(" active", "");
    }
-   document.getElementById("lBreak).style.display = "block";
+   document.getElementById(timer).style.display = "block";
    evt.currentTarget.className += " active";
 }
 
 
-function startTimer(numPomodoros) {
+function startTimer(numPomodoros, timer) {
    let onePomo = 25 * 60000;
    let end = Date.now() + onePomo * numPomodoros;
-
-   setInterval(()=> {
+   if (interval)
+   {
+      clearInterval(interval);
+   }
+   interval = setInterval(()=> {
       let delta = end - Date.now(); 
-      document.getElementById("mins").innerHTML = Math.floor(delta / (1000 * 60) % 60).toString();
-      document.getElementById("secs").innerHTML = Math.floor((delta / 1000) % 60);
+      document.getElementById(timer + "hour").innerHTML = Math.floor(delta / (1000 * 60 * 60));
+      document.getElementById(timer + "mins").innerHTML = Math.floor(delta / (1000 * 60) % 60).toString();
+      document.getElementById(timer + "secs").innerHTML = Math.floor((delta / 1000) % 60);
    }, 100);
+
 }
 

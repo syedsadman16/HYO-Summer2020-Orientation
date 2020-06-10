c6f80458866facb2ed7c7bf0536de6b6dcbbaa52
Michael Focacci <michaelfocacci@computer.lan>
2020-06-10 11:02:50 -0400

    Add pomo functionality in todo.js

--- static/javascript/todo.js
+++ static/javascript/todo.js
@@ -1,4 +1,4 @@
-
+var pomos = 0;
 
 function addListItem(input, time) {
   let list = document.getElementById("list");
@@ -7,7 +7,7 @@ function addListItem(input, time) {
   // Append text input to list item
   item.appendChild(document.createTextNode(input));
 
-  // Create span with time 
+  // Create span with time
   var span = document.createElement("SPAN");
   var timeText = document.createTextNode(time + " minutes");
   span.className = "time";
@@ -22,19 +22,25 @@ function addListItem(input, time) {
 }
 
 
+function addPomoButtonClicked() {
+  pomos += 1;
+}
+
+
 function addTaskButtonClicked() {
   let text = document.getElementById("newTask").value;
   console.log(text);
   if (text != "") {
-    
+
     /* Add pomodoro time clicked logic here */
 
-    addListItem(text, 25); // Default is 25, pass in pomo time instead
+    addListItem(text, pomos*25); // each pomo represents 25 minutes
     document.getElementById("newTask").value = "";
   }
   else {
     alert("Field cannot be empty");
   }
+  pomos = 0; // resets pomo count to 0
   return;
 }
 
@@ -42,7 +48,7 @@ function addTaskButtonClicked() {
 function remove() {
   document.getElementById("list").addEventListener("click",function(e) {
     if (e.target.tagName.toUpperCase() == "LI") {
-      e.target.parentNode.removeChild(e.target); 
+      e.target.parentNode.removeChild(e.target);
     }
   });
 }
